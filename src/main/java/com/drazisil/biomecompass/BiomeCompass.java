51810019d7214d39932b9338754a630a20d6acb9
Joseph Becher <jwbecher@gmail.com>
2015-01-30 05:35:21 -0500

    Now has an item, per #1. It's not connected to anything, and is in fact a renamed vanilla compass, but it's there and it has a name and texture

--- src/main/java/com/drazisil/biomecompass/BiomeCompass.java
+++ src/main/java/com/drazisil/biomecompass/BiomeCompass.java
@@ -1,21 +1,34 @@
 package com.drazisil.biomecompass;
 
 import com.drazisil.biomecompass.commands.BiomeCompassCommand;
+import com.drazisil.biomecompass.items.ItemBiomeCompass;
 import com.drazisil.biomecompass.proxy.BCCommonProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
-@Mod(modid = BiomeCompass.MODID, version = BiomeCompass.VERSION)
+@Mod(modid = BiomeCompass.MODID, name = BiomeCompass.NAME, version = BiomeCompass.VERSION)
 public class BiomeCompass
 {
-    public static final String MODID = "BiomeCompass";
+    public static final String MODID = "biomecompass";
+    public static final String NAME = "Biome Compass";
     public static final String VERSION = "1.0";
 
     @SidedProxy(clientSide = "com.drazisil.biomecompass.proxy.BCClientProxy", serverSide = "com.drazisil.biomecompass.proxy.BCDedicatedServerProxy")
     public static BCCommonProxy proxy;
+
+    @EventHandler
+    public void preinit(FMLPreInitializationEvent event){
+        //MLCommonHandler.instance().bus().register(handler);
+        //MinecraftForge.EVENT_BUS.register(handler);
+
+        ItemBiomeCompass biomeCompassItem = new ItemBiomeCompass();
+        GameRegistry.registerItem(biomeCompassItem, BiomeCompass.MODID + "_biomeCompass");
+    }
 
 
     @EventHandler
