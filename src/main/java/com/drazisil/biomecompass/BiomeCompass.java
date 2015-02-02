e3b710fabb95d08c8e104e904e99468f4efe31ea
Joseph Becher <jwbecher@gmail.com>
2015-02-02 00:30:00 -0500

    v1.0.1 custom texture

--- src/main/java/com/drazisil/biomecompass/BiomeCompass.java
+++ src/main/java/com/drazisil/biomecompass/BiomeCompass.java
@@ -35,7 +35,7 @@ public class BiomeCompass
 {
     public static final String MODID = "biomecompass";
     public static final String NAME = "Biome Compass";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
 
     @SidedProxy(clientSide = "com.drazisil.biomecompass.proxy.BCClientProxy", serverSide = "com.drazisil.biomecompass.proxy.BCDedicatedServerProxy")
     public static BCCommonProxy proxy;
